79269f181e085595b2ec17cb2359b3730479e715
Andre Reus <andrereus@users.noreply.github.com>
2022-06-25 11:43:57 +0200

    Read from database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from "next";
 import firebaseClient from "../firebase/client";
 import { getAuth, signOut } from "firebase/auth";
+import { ref, getDatabase } from "firebase/database";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useList } from "react-firebase-hooks/database";
 import { useRouter } from "next/router";
 
 const auth = getAuth(firebaseClient);
+const database = getDatabase(firebaseClient);
 
 const Home: NextPage = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, user, authloading, autherror] =
+    useSignInWithGoogle(auth);
+  const [snapshots, dbloading, dberror] = useList(
+    ref(database, user?.user.uid + "/ownFood")
+  );
   const router = useRouter();
 
   const logout = () => {
@@ -15,14 +22,14 @@ const Home: NextPage = () => {
     router.reload();
   };
 
-  if (error) {
+  if (autherror) {
     return (
       <div>
-        <p>Error: {error.message}</p>
+        <p>Error: {autherror.message}</p>
       </div>
     );
   }
-  if (loading) {
+  if (authloading) {
     return (
       <div>
         <p>Loading ...</p>
@@ -36,6 +43,33 @@ const Home: NextPage = () => {
         <button onClick={() => logout()} className="btn">
           Sign Out
         </button>
+        <p>
+          {dberror && <strong>Error: {dberror.message}</strong>}
+          {dbloading && <span>Loading ...</span>}
+
+          {!dbloading && snapshots && (
+            <div className="overflow-x-auto">
+              <table className="table w-full">
+                <thead>
+                  <tr>
+                    <th>Icon</th>
+                    <th>Name</th>
+                    <th>Phe</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {snapshots.map((data) => (
+                    <tr key={data.key}>
+                      <td>{data.val().icon}</td>
+                      <td>{data.val().name}</td>
+                      <td>{data.val().phe}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </p>
       </div>
     );
   }
